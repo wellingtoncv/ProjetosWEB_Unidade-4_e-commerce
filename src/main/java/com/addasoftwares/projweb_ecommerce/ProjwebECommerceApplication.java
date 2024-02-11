734ebab8a63690fde3c1ee8b31bfb8607e037d2b
Wellingtoncvs <wellington.santos@addasoftwares.com>
2024-02-11 11:40:02 -0300

    inicio do projeto

---
+++ src/main/java/com/addasoftwares/projweb_ecommerce/ProjwebECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.addasoftwares.projweb_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjwebECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjwebECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addasoftwares/projweb_ecommerce/ProjwebECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addasoftwares.projweb_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjwebECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
